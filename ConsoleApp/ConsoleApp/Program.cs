3416a0f894370ec35ca9878b38f2ef6bf75fac5f
Dhanya Simon <simondhanya@gmail.com>
2022-06-16 11:07:24 -0400

    addition

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp
+{
+     class Program
+    {
+             
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sring");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            Addition();
+        }
+
+        public static void Addition()
+        {
+            Console.WriteLine("Enter 2 numbers to add");
+            Console.WriteLine("Enter first number to add");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number to add");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+        }
+
+    }
+}
